c21b5a2f20cef3510263b324339179903a524de5
Josh Chapman <joshuahchapman@users.noreply.github.com>
2019-01-18 22:30:01 -0800

    add ebird module

---
+++ ebird.py
@@ -0,0 +1,151 @@
+"""
+Utilities for interacting with the eBird APIs
+
+See the API documentation here:
+  https://documenter.getpostman.com/view/664302/ebird-api-20/2HTbHW
+"""
+
+import requests
+import pandas as pd
+
+API_ROOT = "https://ebird.org/ws2.0"
+LISTS_ENDPOINT = "/product/lists"
+OBS_ENDPOINT = "/data/obs"
+REGION_ENDPOINT = "/ref/region"
+CHECKLIST_ENDPOINT = "/product/checklist"
+
+
+class EbirdConn:
+
+    """
+    Utilities for interacting with the eBird APIs
+    """
+
+    def __init__(self, api_token):
+
+        self.api_token = api_token
+
+    def get_recent_observations_for_region(self, region_code, days_back=14, max_results=0):
+        """NB: The eBird endpoint returns each taxon only once."""
+
+        api_params = {"key": self.api_token, "back": days_back, "cat": "species"}
+
+        if max_results != 0:
+            api_params["maxResults"] = max_results
+
+        obs = requests.get(API_ROOT + OBS_ENDPOINT + "/" + region_code + "/recent", params=api_params)
+        data = obs.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
+
+    def get_recent_notable_observations_for_region(self, region_code, days_back=0, max_results=0):
+
+        """
+        If days_back is 0 or not set, uses eBird's max allowed value.
+        """
+
+        url = API_ROOT + OBS_ENDPOINT + "/" + region_code + "/recent/notable"
+
+        api_params = {
+            "key": self.api_token,
+            "back": 30 if days_back == 0 else days_back
+        }
+
+        if max_results != 0:
+            api_params["maxResults"] = max_results
+
+        obs = requests.get(url=url, params=api_params)
+        data = obs.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
+
+    def get_recent_observations_by_lat_long(self, latitude, longitude, distance=25,
+                                            days_back=14, max_results=0):
+
+        api_params = {"key": self.api_token, "lat": latitude, "lng": longitude,
+                      "dist": distance, "back": days_back, "cat": "species"}
+
+        if max_results != 0:
+            api_params["maxResults"] = max_results
+
+        obs = requests.get(API_ROOT + OBS_ENDPOINT + "/geo/recent", params=api_params)
+        data = obs.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
+
+    def get_recent_observations_for_region_for_species(self, region_code,
+                                                       species_code, days_back=14, max_results=0):
+
+        url = API_ROOT + OBS_ENDPOINT + "/" + region_code + "/recent/" + species_code
+
+        api_params = {"key": self.api_token, "back": days_back}
+
+        if max_results != 0:
+            api_params["maxResults"] = max_results
+
+        obs = requests.get(url, params=api_params)
+        data = obs.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
+
+    def get_recent_checklists_for_region(self, region_code, max_results=0):
+
+        """
+        If no value is passed for max_results, uses eBird's max allowed value.
+        """
+
+        url = API_ROOT + LISTS_ENDPOINT + "/" + region_code
+
+        api_params = {
+            "key": self.api_token,
+            "maxResults": 200 if max_results == 0 else max_results
+        }
+
+        obs = requests.get(url, params=api_params)
+        data = obs.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
+
+    def get_checklists_for_date(self, region_code, year, month, day, max_results=0):
+
+        # no zero-padding for month and day (e.g. year=2019, month=1, day=1)
+        url = API_ROOT + LISTS_ENDPOINT + "/" + region_code + "/" + str(year) + "/" + str(month) + "/" + str(day)
+
+        api_params = {
+            "key": self.api_token,
+            "maxResults": 200 if max_results == 0 else max_results
+        }
+
+        obs = requests.get(url, params=api_params)
+        data = obs.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
+
+    def get_checklist_details(self, submission_id):
+
+        api_params = {"key": self.api_token}
+
+        url = API_ROOT + CHECKLIST_ENDPOINT + "/view/" + submission_id
+
+        checklist_details = requests.get(url, params=api_params)
+        data = checklist_details.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
+
+    def get_county_codes_for_region(self, region_code):
+
+        api_params = {"key": self.api_token}
+
+        region_list = requests.get(API_ROOT + REGION_ENDPOINT + "/list/subnational2/" +
+                                   region_code + ".json", params=api_params)
+        data = region_list.json()
+        df = pd.DataFrame.from_dict(data)
+
+        return df
--- server.py
+++ server.py
@@ -1,37 +1,25 @@
 import os
-# import json
 from flask import Flask, request, make_response, Response
-# from tokens import SLACK_BOT_TOKEN, SLACK_VERIFICATION_TOKEN
 from slackclient import SlackClient
-# from slashCommand import *
+import ebird
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+EBIRD_TOKEN = os.environ["EBIRD_TOKEN"]
+
 slack_client = SlackClient(SLACK_BOT_TOKEN)
+
 app = Flask(__name__)
 
-# commander = Slash("Hey there! It works.")
-
 # TODO: Add checks for all responses from slack api calls
-
-
-# def verify_slack_token(request_token):
-#     if SLACK_VERIFICATION_TOKEN != request_token:
-#         print("Error: invalid verification token!")
-#         print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
-#         return make_response("Request contains invalid Slack verification token", 403)
 
 
 @app.route("/slack/test", methods=["POST"])
 def command():
 
     msg = request.form
-
     print(msg)
 
-#    channel_id = msg['user_id'] if msg['channel_name'] == 'directmessage' else msg['channel_id']
-
     channel_id = msg['channel_id']
-    print(channel_id)
 
     # send channel a message
     channel_msg = slack_client.api_call (
