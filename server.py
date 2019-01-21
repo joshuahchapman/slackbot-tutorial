7f000c89b929058d12ee824f64981caa01cf6006
Josh Chapman <joshuahchapman@users.noreply.github.com>
2019-01-21 14:20:20 -0800

    move command-specific validation earlier

--- server.py
+++ server.py
@@ -14,7 +14,10 @@ ebird_client = EbirdClient(EBIRD_TOKEN)
 app = Flask(__name__)
 
 # list of accepted commands
-VALID_COMMANDS = ['recent', 'recent-notable']
+COMMAND_PARAMS = {
+    'recent': ['latitude', 'longitude'],
+    'recent-notable': ['latitude', 'longitude']
+}
 RESTRICTED_COMMANDS = {
     'recent': ['directmessage', 'bot-test'],
     'recent-notable': ['directmessage', 'bot-test']
@@ -25,10 +28,15 @@ def parse_parameters(parameter_list):
 
     cmd = parameter_list.pop(0)
 
-    if cmd not in VALID_COMMANDS:
+    if cmd not in COMMAND_PARAMS.keys():
         valid = False
         validation_message = 'Sorry, I don''t recognize the command _' + cmd + '_. ' + \
-            'These are the commands I know: _' + '_, _'.join(VALID_COMMANDS) + '_.'
+            'These are the commands I know: _' + '_, _'.join(COMMAND_PARAMS.keys()) + '_.'
+
+    elif len(parameter_list) != len(COMMAND_PARAMS[cmd]):
+        valid = False
+        validation_message = 'Looks like you have the wrong number of inputs.\n' \
+            + 'Expected format: `/ebird ' + cmd + ' ' + ' '.join(COMMAND_PARAMS[cmd]) + '`.'
 
     else:
         valid = True
@@ -43,27 +51,22 @@ def handle_command(cmd, cmd_params, channel_id):
 
     if cmd == 'recent':
 
-        if len(cmd_params) != 2:
-            return_message = 'Looks like you have the wrong number of inputs.\n' \
-                    + 'Example of the expected format: `/ebird recent 38.9403316 -74.9212611`.'
+        lat = cmd_params[0]
+        long = cmd_params[1]
+
+        df = ebird_client.get_recent_observations_by_lat_long(lat, long, distance=8, days_back=3)
+
+        if df.empty or 'errors' in df.columns:
+            return_message = 'eBird returned no observations near latitude ' + lat + ', longitude ' + long
 
         else:
-            lat = cmd_params[0]
-            long = cmd_params[1]
-
-            df = ebird_client.get_recent_observations_by_lat_long(lat, long, distance=8, days_back=3)
-
-            if df.empty or 'errors' in df.columns:
-                return_message = 'eBird returned no observations near latitude ' + lat + ', longitude ' + long
-
-            else:
-                return_message = ''
-                for index, row in df.iterrows():
-                    # Format the datetime nicely for display.
-                    pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
-                        '%-m/%-d at %-I:%M %p')
-                    return_message = return_message + '*' + row['comName'] + '*, ' + \
-                        row['locName'] + ', on ' + pretty_dtm + '\n'
+            return_message = ''
+            for index, row in df.iterrows():
+                # Format the datetime nicely for display.
+                pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
+                    '%-m/%-d at %-I:%M %p')
+                return_message = return_message + '*' + row['comName'] + '*, ' + \
+                    row['locName'] + ', on ' + pretty_dtm + '\n'
 
         # send channel a message
         channel_msg = slack_client.api_call(
@@ -76,27 +79,22 @@ def handle_command(cmd, cmd_params, channel_id):
 
     if cmd == 'recent-notable':
 
-        if len(cmd_params) != 2:
-            return_message = 'Looks like you have the wrong number of inputs.\n' \
-                    + 'The expected format is `/ebird recent-notable 38.9403316 -74.9212611`.'
+        lat = cmd_params[0]
+        long = cmd_params[1]
+
+        df = ebird_client.get_recent_notable_observations_by_lat_long(lat, long, distance=8, days_back=3)
+
+        if df.empty or 'errors' in df.columns:
+            return 'eBird returned no notable observations near latitude ' + lat + ', longitude ' + long
 
         else:
-            lat = cmd_params[0]
-            long = cmd_params[1]
-
-            df = ebird_client.get_recent_notable_observations_by_lat_long(lat, long, distance=8, days_back=3)
-
-            if df.empty or 'errors' in df.columns:
-                return 'eBird returned no notable observations near latitude ' + lat + ', longitude ' + long
-
-            else:
-                return_message = ''
-                for index, row in df.iterrows():
-                    # Format the datetime nicely for display.
-                    pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
-                        '%-m/%-d at %-I:%M %p')
-                    return_message = return_message + '*' + row['comName'] + '*, ' + \
-                        row['locName'] + ', on ' + pretty_dtm + '\n'
+            return_message = ''
+            for index, row in df.iterrows():
+                # Format the datetime nicely for display.
+                pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
+                    '%-m/%-d at %-I:%M %p')
+                return_message = return_message + '*' + row['comName'] + '*, ' + \
+                    row['locName'] + ', on ' + pretty_dtm + '\n'
 
         # send channel a message
         channel_msg = slack_client.api_call(
@@ -114,23 +112,25 @@ def command():
     msg = request.form
     print(msg)
 
+    channel_id = msg['channel_id']
+    channel_name = msg['channel_name']
     full_command = msg['text']
+    cmd = full_command[0]
+
+    if cmd in RESTRICTED_COMMANDS.keys():
+        if channel_name not in RESTRICTED_COMMANDS[cmd]:
+            return make_response(
+                'Sorry, command _' + cmd + '_ is not allowed in this channel. ' +
+                'Please try it in a direct message (e.g. with yourself).',
+                200)
+
+    # Validate parameters
     params_valid, validation_message, cmd, cmd_parameters = parse_parameters(full_command.split())
 
     if not params_valid:
         return make_response(validation_message, 200)
 
     else:
-        channel_id = msg['channel_id']
-        channel_name = msg['channel_name']
-
-        if cmd in RESTRICTED_COMMANDS.keys():
-            if channel_name not in RESTRICTED_COMMANDS[cmd]:
-                return make_response(
-                    'Sorry, command _' + cmd + '_ is not allowed in this channel. ' +
-                    'Please try it in a direct message (e.g. with yourself).',
-                    200)
-
         thread = Thread(target=handle_command, args=(cmd, cmd_parameters, channel_id))
         thread.start()
 
