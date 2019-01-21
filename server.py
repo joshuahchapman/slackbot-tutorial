ece66a8963e9576a2c439c6cb148f831d10f2414
Josh Chapman <joshuahchapman@users.noreply.github.com>
2019-01-21 13:06:10 -0800

    minor message formatting

--- server.py
+++ server.py
@@ -28,8 +28,8 @@ def parse_parameters(parameter_list):
 
     if cmd not in VALID_COMMANDS:
         valid = False
-        validation_message = 'Sorry, I don''t recognize the command ' + cmd + '. ' + \
-            'These are the commands I know: ' + ", ".join(VALID_COMMANDS)
+        validation_message = 'Sorry, I don''t recognize the command _' + cmd + '_. ' + \
+            'These are the commands I know: _' + '_, '.join(VALID_COMMANDS) + '_'
 
     else:
         valid = True
