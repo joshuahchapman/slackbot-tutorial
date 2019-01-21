9cb46716a539536a184da2517e0d952b175d38ba
Josh Chapman <joshuahchapman@users.noreply.github.com>
2019-01-21 12:48:22 -0800

    standardize handling of commands

--- server.py
+++ server.py
@@ -34,38 +34,34 @@ def parse_parameters(parameter_list):
     return valid, validation_message, cmd, parameter_list
 
 
-def handle_command(cmd, cmd_params, channel_id, response_url):
+def handle_command(cmd, cmd_params, channel_id):
 
     print('Handling command...')
 
     if cmd == 'recent':
 
         if len(cmd_params) != 2:
-            return 'Looks like you have the wrong number of inputs.\n' \
+            return_message = 'Looks like you have the wrong number of inputs.\n' \
                     + 'The expected format is `/slashtest recent [latitude] [longitude]`.\n' \
                     + 'For example: `/slashtest recent 38.9403316 -74.9212611`'
 
-        lat = cmd_params[0]
-        long = cmd_params[1]
+        else:
+            lat = cmd_params[0]
+            long = cmd_params[1]
 
-        df = ebird_client.get_recent_observations_by_lat_long(lat, long, distance=8, days_back=3)
+            df = ebird_client.get_recent_observations_by_lat_long(lat, long, distance=8, days_back=3)
 
-        if df.empty or 'errors' in df.columns:
-            return 'eBird returned no observations near latitude ' + lat + ', longitude ' + long
+            if df.empty or 'errors' in df.columns:
+                return_message = 'eBird returned no observations near latitude ' + lat + ', longitude ' + long
 
-        return_message = ''
-        for index, row in df.iterrows():
-            # Format the datetime nicely for display.
-            pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
-                '%-m/%-d at %-I:%M %p')
-            return_message = return_message + '*' + row['comName'] + '*, ' + \
-                row['locName'] + ', on ' + pretty_dtm + '\n'
-
-#        api_params = {
-#            "token": SLACK_BOT_TOKEN,
-#            "text": return_message
-#        }
-#        requests.post(url=response_url, params=api_params)
+            else:
+                return_message = ''
+                for index, row in df.iterrows():
+                    # Format the datetime nicely for display.
+                    pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
+                        '%-m/%-d at %-I:%M %p')
+                    return_message = return_message + '*' + row['comName'] + '*, ' + \
+                        row['locName'] + ', on ' + pretty_dtm + '\n'
 
         # send channel a message
         channel_msg = slack_client.api_call(
@@ -79,29 +75,36 @@ def handle_command(cmd, cmd_params, channel_id, response_url):
     if cmd == 'recent-notable':
 
         if len(cmd_params) != 2:
-            return 'Looks like you have the wrong number of inputs.\n' \
+            return_message = 'Looks like you have the wrong number of inputs.\n' \
                     + 'The expected format is `/slashtest recent-notable [latitude] [longitude]`.\n' \
                     + 'For example: `/slashtest recent-notable 38.9403316 -74.9212611`'
 
-        lat = cmd_params[0]
-        long = cmd_params[1]
+        else:
+            lat = cmd_params[0]
+            long = cmd_params[1]
 
-        # TO DO: validate coordinates?
+            df = ebird_client.get_recent_notable_observations_by_lat_long(lat, long, distance=8, days_back=3)
 
-        df = ebird_client.get_recent_notable_observations_by_lat_long(lat, long, distance=8, days_back=3)
+            if df.empty or 'errors' in df.columns:
+                return 'eBird returned no notable observations near latitude ' + lat + ', longitude ' + long
 
-        if df.empty or 'errors' in df.columns:
-            return 'eBird returned no notable observations near latitude ' + lat + ', longitude ' + long
+            else:
+                return_message = ''
+                for index, row in df.iterrows():
+                    # Format the datetime nicely for display.
+                    pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
+                        '%-m/%-d at %-I:%M %p')
+                    return_message = return_message + '*' + row['comName'] + '*, ' + \
+                        row['locName'] + ', on ' + pretty_dtm + '\n'
 
-        return_message = ''
-        for index, row in df.iterrows():
-            # Format the datetime nicely for display.
-            pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
-                '%-m/%-d at %-I:%M %p')
-            return_message = return_message + '*' + row['comName'] + '*, ' + \
-                row['locName'] + ', on ' + pretty_dtm + '\n'
+        # send channel a message
+        channel_msg = slack_client.api_call(
+            "chat.postMessage",
+            channel=channel_id,
+            text=return_message
+        )
 
-        return return_message
+        return
 
 
 @app.route("/slack/test", methods=["POST"])
@@ -113,11 +116,10 @@ def command():
     full_command = msg['text']
     params_valid, validation_message, cmd, cmd_parameters = parse_parameters(full_command.split())
 
-# Thread stuff
     channel_id = msg['channel_id']
-    response_url = msg['response_url']
-    thread = Thread(target=handle_command, args=(cmd, cmd_parameters, channel_id, response_url))
+    thread = Thread(target=handle_command, args=(cmd, cmd_parameters, channel_id))
     thread.start()
+
     return make_response(validation_message, 200)
 
 
